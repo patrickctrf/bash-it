d8fd0b5da561d188f085d9300ce787f99b2b57e1
Patrick de Carvalho Tavares Rezende Ferreira <ra175480@students.ic.unicamp.br>
2020-11-14 19:35:48 -0300

    Minhas próprias edições. Patrick

--- themes/sexy/sexy.theme_editado.bash
+++ themes/sexy/sexy.theme_editado.bash
@@ -11,23 +11,17 @@ SEXY_THEME_SHOW_PYTHON="${SEXY_THEME_SHOW_PYTHON:=false}"
 
 if tput setaf 1 &> /dev/null; then
     if [[ $(tput colors) -ge 256 ]] 2>/dev/null; then
-      MAGENTA=$(tput setaf 9)
-      ORANGE=$(tput setaf 172)
-      GREEN=$(tput setaf 190)
+      MAGENTA=$(tput setaf 2) #154 190 112 2
+      ORANGE=$(tput setaf 2) #154 190 112 2
+      GREEN=$(tput setaf 12) #117 68 
       PURPLE=$(tput setaf 141)
-      WHITE=$(tput setaf 0)
-      GREEN_OLD=$(tput setaf 2)
-      BLUE_OLD=$(tput setaf 4)
-      WHITE_OLD=$(tput setaf 255)
+      WHITE=$(tput setaf 240) # 0
     else
       MAGENTA=$(tput setaf 5)
       ORANGE=$(tput setaf 4)
       GREEN=$(tput setaf 2)
       PURPLE=$(tput setaf 1)
       WHITE=$(tput setaf 7)
-      GREEN_OLD=$GREEN
-      BLUE_OLD=$BLUE
-      WHITE_OLD=$WHITE
     fi
     BOLD=$(tput bold)
     RESET=$(tput sgr0)
@@ -39,9 +33,6 @@ else
     WHITE="\033[1;37m"
     BOLD=""
     RESET="\033[m"
-    GREEN_OLD=$GREEN
-    BLUE_OLD=$BLUE
-    WHITE_OLD=$WHITE
 fi
 
 parse_git_dirty () {
@@ -55,10 +46,6 @@ env_prompt () {
 }
 
 function prompt_command() {
-  # classic like
-  # PS1="\[${BOLD}${GREEN_OLD}\]\u@\[$GREEN_OLD\]\h\[$WHITE_OLD\]:\[$BLUE_OLD\]\w\$([[ -n \$(git branch 2> /dev/null) ]] && echo \"\[$PURPLE\] ⎇  \")\[$PURPLE\]\$(parse_git_branch)\[$WHITE\]\n\$ \[$RESET\]"
-  
-  # sexy
   PS1="\[${BOLD}${MAGENTA}\]\u \[$WHITE\]at \[$ORANGE\]\h \[$WHITE\]in \[$GREEN\]\w\[$WHITE\]\$([[ -n \$(git branch 2> /dev/null) ]] && echo \" on \[$PURPLE\]⎇  \")\[$PURPLE\]\$(parse_git_branch)\[$WHITE\]\n\$ \[$RESET\]"
 
   if [ "$SEXY_THEME_SHOW_PYTHON" = true ] ; then
--- themes/sexy/sexy.theme_original.bash
+++ themes/sexy/sexy.theme_original.bash
@@ -16,18 +16,12 @@ if tput setaf 1 &> /dev/null; then
       GREEN=$(tput setaf 190)
       PURPLE=$(tput setaf 141)
       WHITE=$(tput setaf 0)
-      GREEN_OLD=$(tput setaf 2)
-      BLUE_OLD=$(tput setaf 4)
-      WHITE_OLD=$(tput setaf 255)
     else
       MAGENTA=$(tput setaf 5)
       ORANGE=$(tput setaf 4)
       GREEN=$(tput setaf 2)
       PURPLE=$(tput setaf 1)
       WHITE=$(tput setaf 7)
-      GREEN_OLD=$GREEN
-      BLUE_OLD=$BLUE
-      WHITE_OLD=$WHITE
     fi
     BOLD=$(tput bold)
     RESET=$(tput sgr0)
@@ -39,9 +33,6 @@ else
     WHITE="\033[1;37m"
     BOLD=""
     RESET="\033[m"
-    GREEN_OLD=$GREEN
-    BLUE_OLD=$BLUE
-    WHITE_OLD=$WHITE
 fi
 
 parse_git_dirty () {
@@ -55,10 +46,6 @@ env_prompt () {
 }
 
 function prompt_command() {
-  # classic like
-  # PS1="\[${BOLD}${GREEN_OLD}\]\u@\[$GREEN_OLD\]\h\[$WHITE_OLD\]:\[$BLUE_OLD\]\w\$([[ -n \$(git branch 2> /dev/null) ]] && echo \"\[$PURPLE\] ⎇  \")\[$PURPLE\]\$(parse_git_branch)\[$WHITE\]\n\$ \[$RESET\]"
-  
-  # sexy
   PS1="\[${BOLD}${MAGENTA}\]\u \[$WHITE\]at \[$ORANGE\]\h \[$WHITE\]in \[$GREEN\]\w\[$WHITE\]\$([[ -n \$(git branch 2> /dev/null) ]] && echo \" on \[$PURPLE\]⎇  \")\[$PURPLE\]\$(parse_git_branch)\[$WHITE\]\n\$ \[$RESET\]"
 
   if [ "$SEXY_THEME_SHOW_PYTHON" = true ] ; then
